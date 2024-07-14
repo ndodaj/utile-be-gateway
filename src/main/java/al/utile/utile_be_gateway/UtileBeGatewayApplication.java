c2a84ff3b6923192f755c813abcf12c395653080
nidodaj <nikoll.dodaj@eng.it>
2024-07-15 00:19:58 +0200

    init

---
+++ src/main/java/al/utile/utile_be_gateway/UtileBeGatewayApplication.java
@@ -0,0 +1,13 @@
+package al.utile.utile_be_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtileBeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtileBeGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/utile/utile_be_gateway/UtileBeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package al.utile.utile_be_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtileBeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
