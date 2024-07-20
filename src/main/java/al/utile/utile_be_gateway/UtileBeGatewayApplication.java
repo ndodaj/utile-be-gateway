d0c28807ffa496b00a4d96e3bf43c023928eebb5
nidodaj <nikoll.dodaj@eng.it>
2024-07-20 12:44:54 +0200

    adding bussiness logic

--- src/main/java/al/utile/utile_be_gateway/UtileBeGatewayApplication.java
+++ src/main/java/al/utile/utile_be_gateway/UtileBeGatewayApplication.java
@@ -2,8 +2,10 @@ package al.utile.utile_be_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UtileBeGatewayApplication {
 
 	public static void main(String[] args) {
