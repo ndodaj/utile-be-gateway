d5a325705c6096076136ae147787ac0b18aa8ea7
nidodaj <nikoll.dodaj@eng.it>
2024-07-30 01:36:30 +0200

    Enable Authorize button in springdoc-openapi-ui for Bearer Token Authentication

---
+++ src/main/java/al/utile/utile_be_gateway/config/OpenApi30Config.java
@@ -0,0 +1,19 @@
+package al.utile.utile_be_gateway.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Utile API GATEWAY", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApi30Config {
+
+}
