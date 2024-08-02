7c57700b69d7c9ea4025ba9d7fff521cb2a39376
nidodaj <nikoll.dodaj@eng.it>
2024-08-03 00:52:50 +0200

    fix

--- src/main/java/al/utile/utile_be_gateway/controller/RoleController.java
+++ src/main/java/al/utile/utile_be_gateway/controller/RoleController.java
@@ -1,23 +1,26 @@
 package al.utile.utile_be_gateway.controller;
 
-import al.utile.utile_be_gateway.security.JwtUtil;
 import al.utile.utile_be_gateway.service.ServiceProvider;
-import al.utile.utile_common.utile.dto.AuthenticationResponse;
 import al.utile.utile_common.utile.dto.RoleDto;
 import al.utile.utile_common.utile.service.UtileServices;
-import com.fasterxml.jackson.core.type.TypeReference;
-import org.springframework.cloud.client.ServiceInstance;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
-@RestController("api/roles")
+@RestController
+@RequestMapping("api/roles")
+@Tag(name = "Role Controller", description = "Role APIs")
 public class RoleController {
 
     private final RestClient restClient;
@@ -32,14 +35,62 @@ public class RoleController {
 
     @GetMapping
     ResponseEntity<List<RoleDto>> getRoles() {
-        ServiceInstance utileAppInstance = serviceProvider.serviceInstances.get(UtileServices.UTILE);
-
         List<RoleDto> body = restClient.get()
-                .uri(utileAppInstance.getUri() + "/roles")
+                .uri(utileURI())
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {
                 });
         return ResponseEntity.ok(body);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoleDto> getRole(@PathVariable("id") String id) {
+        return ResponseEntity.ok(
+                restClient.get()
+                        .uri(utileURI())
+                        .retrieve()
+                        .body(new ParameterizedTypeReference<>() {
+                        })
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<RoleDto> createRole(@RequestBody RoleDto role) {
+        return ResponseEntity.ok(
+                restClient.post()
+                        .uri(utileURI())
+                        .body(role)
+                        .retrieve()
+                        .body(new ParameterizedTypeReference<>() {
+                        })
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RoleDto> updateRole(@PathVariable("id") String id, @RequestBody RoleDto role) {
+        return ResponseEntity.ok(
+                restClient.put()
+                        .uri(utileURI() + "/" + id)
+                        .body(role)
+                        .retrieve()
+                        .body(new ParameterizedTypeReference<>() {
+                        })
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteRole(@PathVariable("id") String id) {
+        return ResponseEntity.ok(
+                restClient.delete()
+                        .uri(utileURI() + "/" + id)
+                        .retrieve()
+                        .body(new ParameterizedTypeReference<>() {
+                        })
+        );
+    }
+
+    private String utileURI() {
+        return serviceProvider.serviceInstances.get(UtileServices.UTILE) + "/roles";
     }
 
 }
